6d71a36ab9c1348f2ff841e9ff5a5176f4e7d2c4
manankhndlwl <162955550+manankhndlwl@users.noreply.github.com>
2025-07-05 16:45:06 +0530

    Feat: moved to react native 0.80 and removed stripe , mapbox and react-native-voice package

--- src/config.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const BASE_URL = "http://localhost:8000/";
-
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-});
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    };
-  
